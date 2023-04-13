826c3e5555702aa2cde87c26e870c3461975b5e5
Aly Nathani <alynathani@hotmail.co.uk>
2023-04-13 05:27:19 +0100

    added starter template and added logos, buttons and navbar

---
+++ cortex-ai-website/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Image from 'next/image'
+import HeroSection from "@/components/HeroSection"
+import { Inter } from 'next/font/google'
+
+export default function Home() {
+  return (
+    <>
+     <HeroSection />
+    </>
+  )
+}
