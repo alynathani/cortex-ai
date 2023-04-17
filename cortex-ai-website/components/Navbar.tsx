99ece48dfec2cd60aab7336996c049bc9f9bfb81
Aly Nathani <alynathani@hotmail.co.uk>
2023-04-17 12:50:28 +0100

    fixed navbar logo

--- cortex-ai-website/components/Navbar.tsx
+++ cortex-ai-website/components/Navbar.tsx
@@ -14,6 +14,7 @@ import {
   useColorModeValue,
   useBreakpointValue,
   useDisclosure,
+  Image,
   Center,
 } from '@chakra-ui/react';
 import {
@@ -57,7 +58,7 @@ export default function WithSubnavigation() {
             textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
             fontFamily={'heading'}
             color={useColorModeValue('gray.800', 'white')}>
-              <Link href='/'><img style={{height: '40px'}} src='./NavBarLogo.png'></img></Link>
+              <Link href='/'><Image h={'40px'} src='./NavBarLogo.png' /></Link>
               
           </Text>
           <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
