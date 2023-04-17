8b86f6c5eab20ae8a784a7d327ac3f14c98353f0
Aly Nathani <alynathani@hotmail.co.uk>
2023-04-17 19:21:17 +0100

    omggg

--- cortex-ai-website/app/page.tsx
+++ cortex-ai-website/app/page.tsx
@@ -6,31 +6,12 @@ import { Inter } from 'next/font/google'
 import { GetServerSideProps, GetStaticProps } from 'next'
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-async function getTools() {
-  const res = await fetch(`${`http://cortex-ai-git-developing-alynathani.vercel.app`}/api/getCategories`)
-  return res.json();
-  
-}
+
 
 export default async function Home() {
-  const data: {
-    id: number; name: string; category: string; description: string; rating: number; downloads: number; imageLink: string
-  }[] = await getTools()
-
-  console.log(data);
-  
-  
   return (
-    
     <>
     <HeroSection />
-    <h1>Hi</h1>
-  
-    {/*{data.map((tool) => (
-      <h1 style={{color: "white"}} >{tool.name}</h1>
-    ))}*/}
-     
-     
     </>
   )
 }
