dd933621030189512625cf1bdb0f6e22b7469a34
Aly Nathani <alynathani@hotmail.co.uk>
2023-04-17 12:53:52 +0100

    third time lucky

--- cortex-ai-website/components/Navbar.tsx
+++ cortex-ai-website/components/Navbar.tsx
@@ -58,7 +58,7 @@ export default function WithSubnavigation() {
             textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
             fontFamily={'heading'}
             color={useColorModeValue('gray.800', 'white')}>
-              <Image h={'40px'} src='NavBarLogo.png' />
+              <Link href='/'><Image h={'40px'} src='NavbarLogo.png' /></Link>
               
           </Text>
           <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
