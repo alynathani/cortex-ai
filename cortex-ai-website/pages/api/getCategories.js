deba45ba10512e01476587ca7c9bd7c297dc8351
Aly Nathani <alynathani@hotmail.co.uk>
2023-04-22 23:36:23 +0100

    temporarily using hard coded data

--- cortex-ai-website/pages/api/getCategories.js
+++ cortex-ai-website/pages/api/getCategories.js
@@ -2,7 +2,11 @@ import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
     try{
+        /*
         const data = await prisma.category.findMany()
+        */
+
+        const data = [{"id":1,"name":"Accounting"},{"id":2,"name":"Maths"},{"id":3,"name":"Essay Writing"},{"id":4,"name":"Programming"},{"id":5,"name":"Photo Generation"},{"id":6,"name":"Music Generation"}]
         return res.status(200).json(data);
     } catch (error) {
         return res.status(500).json(error)
@@ -10,3 +14,5 @@ export default async function handler(req, res) {
     
     
 }
+
+[{"id":1,"name":"Accounting"},{"id":2,"name":"Maths"},{"id":3,"name":"Essay Writing"},{"id":4,"name":"Programming"},{"id":5,"name":"Photo Generation"},{"id":6,"name":"Music Generation"}]
