bc6a1c7157150ea865eb1f7bc44614ca5c6dd7ff
Aly Nathani <alynathani@hotmail.co.uk>
2023-04-23 00:05:24 +0100

    again

--- cortex-ai-website/pages/api/getCategories.js
+++ cortex-ai-website/pages/api/getCategories.js
@@ -1,18 +1,8 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-    try{
-        /*
-        const data = await prisma.category.findMany()
-        */
-
-        const data = [{"id":1,"name":"Accounting"},{"id":2,"name":"Maths"},{"id":3,"name":"Essay Writing"},{"id":4,"name":"Programming"},{"id":5,"name":"Photo Generation"},{"id":6,"name":"Music Generation"}]
-        return res.status(200).json(data);
-    } catch (error) {
-        return res.status(500).json(error)
-    }
-    
+    const data = [{"id":1,"name":"Accounting"},{"id":2,"name":"Maths"},{"id":3,"name":"Essay Writing"},{"id":4,"name":"Programming"},{"id":5,"name":"Photo Generation"},{"id":6,"name":"Music Generation"}]
+    return data
     
 }
 
-[{"id":1,"name":"Accounting"},{"id":2,"name":"Maths"},{"id":3,"name":"Essay Writing"},{"id":4,"name":"Programming"},{"id":5,"name":"Photo Generation"},{"id":6,"name":"Music Generation"}]
