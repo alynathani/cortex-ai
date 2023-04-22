1d6e8e1576921382dad9948256e43a0dafc16553
Aly Nathani <alynathani@hotmail.co.uk>
2023-04-23 00:07:56 +0100

    one more time

--- cortex-ai-website/pages/api/getCategories.js
+++ cortex-ai-website/pages/api/getCategories.js
@@ -1,8 +1,7 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-    const data = [{"id":1,"name":"Accounting"},{"id":2,"name":"Maths"},{"id":3,"name":"Essay Writing"},{"id":4,"name":"Programming"},{"id":5,"name":"Photo Generation"},{"id":6,"name":"Music Generation"}]
-    return data
+    res.status(200).json([{"id":1,"name":"Accounting"},{"id":2,"name":"Maths"},{"id":3,"name":"Essay Writing"},{"id":4,"name":"Programming"},{"id":5,"name":"Photo Generation"},{"id":6,"name":"Music Generation"}])
     
 }
 
