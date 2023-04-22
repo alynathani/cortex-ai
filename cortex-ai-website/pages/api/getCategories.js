69289d3a919523da2e9fad0e63964d65ae515f09
Aly Nathani <alynathani@hotmail.co.uk>
2023-04-23 00:18:43 +0100

    reverted back as vercel is bad

--- cortex-ai-website/pages/api/getCategories.js
+++ cortex-ai-website/pages/api/getCategories.js
@@ -1,7 +1,16 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-    res.status(200).json([{"id":1,"name":"Accounting"},{"id":2,"name":"Maths"},{"id":3,"name":"Essay Writing"},{"id":4,"name":"Programming"},{"id":5,"name":"Photo Generation"},{"id":6,"name":"Music Generation"}])
+    try{
+        
+        const data = await prisma.category.findMany()
+        
+
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+    
     
 }
 
