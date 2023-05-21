2fd47347f84f5c7e4750c0a1d040bdd057385dc9
Aly Nathani <alynathani@hotmail.co.uk>
2023-05-21 21:56:20 +0100

    changed cache

--- cortex-ai-website/app/rankings/page.tsx
+++ cortex-ai-website/app/rankings/page.tsx
@@ -21,7 +21,7 @@ type toolListProps = {
 
 async function getTools() {
   const res = await fetch(`${process.env.BASE_URL}/api/getAllTools`, {
-    cache: "default",
+    cache: "no-store",
   });
   if (!res.ok) {
     console.log(res);
