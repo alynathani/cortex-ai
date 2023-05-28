9a644656489bdb5209b1f247f6c2c75d8737223b
Aly Nathani <alynathani@hotmail.co.uk>
2023-05-28 01:35:01 +0100

    promise

--- cortex-ai-website/app/categories/page.tsx
+++ cortex-ai-website/app/categories/page.tsx
@@ -6,7 +6,13 @@ export const metadata = {
   title: 'Cortex AI - Browse'
 }
 
-async function getCategories() {
+type categoryProps = {
+  id: number,
+  name: string,
+  description: string
+}
+
+async function getCategories():Promise<categoryProps> {
   const res = await fetch(`${process.env.BASE_URL}/api/getCategories`, {cache:'no-store'})
   const jsonData = await res.json()
 
