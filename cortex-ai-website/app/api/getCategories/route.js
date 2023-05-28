a8509faceac263b64a4dc0c364a16aa7b4d579d5
Aly Nathani <alynathani@hotmail.co.uk>
2023-05-28 01:54:33 +0100

    again

--- cortex-ai-website/app/api/getCategories/route.js
+++ cortex-ai-website/app/api/getCategories/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import prisma from "../../../lib/prisma"
 
 export async function GET(request, { params }) {
-        const data = await prisma.category.findMany()
+        const data = await prisma.category.findMany({})
         return NextResponse.json(data)
     
 }
